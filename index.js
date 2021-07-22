fdacb67d939021bb6481bcb3e3484466dc7a544f
crestlark <stephanie.lewis.art@gmail.com>
2021-07-21 21:22:41 -0600

    commit tree f415e1960de264d9dfdb9da31516350ceecb61da

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction (createdParameter) {
+    createdParameter();
+}
+function returnsANamedFunction() {
+    return returnsANamedFunction;
+}
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
